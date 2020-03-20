6463f494d99ec4ae844db6883c61ca80f4011a0f
saurabhtandon13 <tandonsa@vmware.com>
2020-03-20 10:14:41 +0530

    added folder application-structure

---
+++ application-structure/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x int = 100
+
+const y = 0
+
+func main() {
+
+	var a int = 7
+	var b float64 = 3.5
+	const c int = 10
+
+	fmt.Println("Hello Go World !!!")
+	fmt.Println(a, b, c)
+}
+
